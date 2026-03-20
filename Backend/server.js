97e4f3f9e61061eb6499c1d4c346d1c3398d7508
itsbishal10 <bishalsharma7777@gmail.com>
2026-03-20 23:53:10 +0530

    fix: dynamic port for render

--- Backend/server.js
+++ Backend/server.js
@@ -6,6 +6,9 @@ const connectToDB = require("./src/config/database")
 connectToDB()
 
 
-app.listen(3000, ()=>{
-    console.log("server is running on port 3000")
-})
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+// app.listen(3000, ()=>{
+//     console.log("server is running on port 3000")
+// })
