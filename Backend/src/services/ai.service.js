b6950f403f03004d317526c51c63bc0c3af31e47
itsbishal10 <bishalsharma7777@gmail.com>
2026-03-23 23:50:27 +0530

    fix: force puppeteer to use bundled chromium executable

--- Backend/src/services/ai.service.js
+++ Backend/src/services/ai.service.js
@@ -119,9 +119,10 @@ Job Description: ${jobDescription}
 async function generatePdfFromHtml(htmlContent) {
     // const browser = await puppeteer.launch()
     const browser = await puppeteer.launch({
-    args: ['--no-sandbox', '--disable-setuid-sandbox'],
-    headless: true
-})
+        executablePath: puppeteer.executablePath(), // FORCE correct chrome
+        args: ['--no-sandbox', '--disable-setuid-sandbox'],
+        headless: true
+    })
     const page = await browser.newPage()
     await page.setContent(htmlContent, { waitUntil: "networkidle0" })
 
